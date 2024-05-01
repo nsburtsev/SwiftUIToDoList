baf10c5fdb82f91025f3c813e42c8f296086a6ef
nsburtsev <burtsev.ns@gmail.com>
2024-05-01 14:30:54 +0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Нюргун on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
