b7a5b64aea8d3353004dea11e1d4169920ddd72a
nsburtsev <burtsev.ns@gmail.com>
2024-05-15 20:44:12 +0600

    Create UI for NewItemView

--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 20)
+            
+            Form {
+                // Title
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                // Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // Button
+                TLButton(
+                    title: "Save",
+                    background: .pink
+                ) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+                }
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text("Please fill in all fields and select due date that is today or newer."))
+            }
+        }
     }
 }
 
 struct NewItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NewItemView()
+        NewItemView(newItemPresented: Binding(get: {
+            return true
+        }, set: { _ in
+            
+        }))
     }
 }
